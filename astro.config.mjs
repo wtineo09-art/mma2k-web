856513e3851c035e9c22bd134aad6c9d55dec328
wtineo09-art <wtineo@gmail.com>
2026-03-10 14:38:06 -0400

    Activado modo servidor para noticias dinamicas

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  integrations: [tailwind()],
+  output: 'server', // Esto activa el modo dinámico
+  adapter: vercel(),
 });
